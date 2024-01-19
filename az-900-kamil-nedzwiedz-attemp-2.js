a8e56e4f691bb21db92dc44f1bfb5b1ed6265284
Artur Wozniak <wozniaka83@gmail.com>
2024-01-19 03:42:20 +0100

    attempt 2, q132

--- az-900-kamil-nedzwiedz-attemp-2.js
+++ az-900-kamil-nedzwiedz-attemp-2.js
@@ -206,203 +206,403 @@ const answers = {
   //   correct: ["local network gateways"],
   //   given: ["virtual network"],
   // },
-  51: {
-    correct: ["c"],
-    given: ["b"],
-  },
-  52: {
-    correct: ["b"],
-    given: ["b"],
-  },
-  53: {
-    correct: ["a"],
-    given: ["a"],
-  },
-  54: {
-    correct: ["a"],
-    given: ["a"],
-  },
-  55: {
+  // 51: {
+  //   correct: ["c"],
+  //   given: ["b"],
+  // },
+  // 52: {
+  //   correct: ["b"],
+  //   given: ["b"],
+  // },
+  // 53: {
+  //   correct: ["a"],
+  //   given: ["a"],
+  // },
+  // 54: {
+  //   correct: ["a"],
+  //   given: ["a"],
+  // },
+  // 55: {
+  //   correct: ["d"],
+  //   given: ["d"],
+  // },
+  // 56: {
+  //   correct: [],
+  //   given: [],
+  // },
+  // 57: {
+  //   correct: ["1:2", "2:3", "3:4", "4:1"],
+  //   given: ["1:2", "2:3", "3:4", "4:1"],
+  // },
+  // 58: {
+  //   correct: ["a", "e"],
+  //   given: ["a", "d"],
+  // },
+  // 59: {
+  //   correct: ["1:4", "2:3", "3:2", "4:1"],
+  //   given: ["1:4", "2:3", "3:2", "4:1"],
+  // },
+  // 60: {
+  //   correct: ["monitor", "marketplace", "advisor"],
+  //   given: ["monitor", "marketplace", "advisor"],
+  // },
+  // 61: {
+  //   correct: ["a"],
+  //   given: ["a"],
+  // },
+  // 62: {
+  //   correct: ["containers"],
+  //   given: ["containers"],
+  // },
+  // 63: {
+  //   correct: ["b"],
+  //   given: ["b"],
+  // },
+  // 64: {
+  //   correct: ["c"],
+  //   given: ["c"],
+  // },
+  // 65: {
+  //   correct: ["azure cosmos db"],
+  //   given: ["azure cosmos db"],
+  // },
+  // 66: {
+  //   correct: ["a"],
+  //   given: ["a"],
+  // },
+  // 67: {
+  //   correct: ["c"],
+  //   given: ["c"],
+  // },
+  // 68: {
+  //   correct: ["c"],
+  //   given: ["c"],
+  // },
+  // 69: {
+  //   correct: ["a", "d"],
+  //   given: ["d", "e"],
+  // },
+  // 70: {
+  //   correct: ["b", "c"],
+  //   given: ["b", "c"],
+  // },
+  // 71: {
+  //   correct: ["product of both"],
+  //   given: ["product of both"],
+  // },
+  // 72: {
+  //   correct: [0, 0, 1],
+  //   given: [0, 1, 1],
+  // },
+  // 73: {
+  //   correct: ["b"],
+  //   given: ["a"],
+  // },
+  // 74: {
+  //   correct: ["must be rehydrated before the data can be accessed"],
+  //   given: ["must be restored before the data can be accessed"],
+  // },
+  // 75: {
+  //   correct: [0, 1, 0],
+  //   given: [0, 1, 1],
+  // },
+  // 76: {
+  //   correct: ["a"],
+  //   given: ["a"],
+  // },
+  // 77: {
+  //   correct: ["b"],
+  //   given: ["b"],
+  // },
+  // 78: {
+  //   correct: ["b"],
+  //   given: ["a"],
+  // },
+  // 79: {
+  //   correct: ["azure cli, azure portal, azure powershell"],
+  //   given: ["azure cli, azure portal, azure powershell"],
+  // },
+  // 80: {
+  //   correct: ["azure resources manager templates"],
+  //   given: ["azure policies"],
+  // },
+  // 81: {
+  //   correct: ["1:2", "2:4", "3:1", "4:3"],
+  //   given: ["1:2", "2:4", "3:1", "4:3"],
+  // },
+  // 82: {
+  //   correct: ["a"],
+  //   given: ["a"],
+  // },
+  // 83: {
+  //   correct: ["1:4", "2:3", "3:1", "4:2"],
+  //   given: ["1:4", "2:3", "3:1", "4:2"],
+  // },
+  // 84: {
+  //   correct: ["b"],
+  //   given: ["b"],
+  // },
+  // 85: {
+  //   correct: ["a"],
+  //   given: ["a"],
+  // },
+  // 86: {
+  //   correct: ["a"],
+  //   given: ["a"],
+  // },
+  // 87: {
+  //   correct: ["1:2", "2:1", "3:4", "4:3"],
+  //   given: ["1:2", "2:1", "3:4", "4:3"],
+  // },
+  // 88: {
+  //   correct: [2, 2],
+  //   given: [2, 2],
+  // },
+  // 89: {
+  //   correct: ["a"],
+  //   given: ["a"],
+  // },
+  // 90: {
+  //   correct: ["a"],
+  //   given: ["a"],
+  // },
+  // 91: {
+  //   correct: ["b"],
+  //   given: ["b"],
+  // },
+  // 92: {
+  //   correct: ["a"],
+  //   given: ["a"],
+  // },
+  // 93: {
+  //   correct: ["a"],
+  //   given: ["a"],
+  // },
+  // 94: {
+  //   correct: [1, 1, 1],
+  //   given: [1, 0, 1],
+  // },
+  // 95: {
+  //   correct: ["a"],
+  //   given: ["a"],
+  // },
+  // 96: {
+  //   correct: ["fault tolerance"],
+  //   given: ["fault tolerance"],
+  // },
+  // 97: {
+  //   correct: ["b"],
+  //   given: ["b"],
+  // },
+  // 98: {
+  //   correct: [1, 0, 0],
+  //   given: [1, 0, 0],
+  // },
+  // 99: {
+  //   correct: [0, 0, 0],
+  //   given: [1, 1, 0],
+  // },
+  // 100: {
+  //   correct: [1, 1, 0],
+  //   given: [1, 1, 0],
+  // },
+  101: {
     correct: ["d"],
     given: ["d"],
   },
-  56: {
-    correct: [],
-    given: [],
+  102: {
+    correct: ["d"],
+    given: ["d"],
   },
-  57: {
-    correct: ["1:2", "2:3", "3:4", "4:1"],
-    given: ["1:2", "2:3", "3:4", "4:1"],
+  103: {
+    correct: [0, 1, 1],
+    given: [0, 1, 1],
   },
-  58: {
-    correct: ["a", "e"],
-    given: ["a", "d"],
+  104: {
+    correct: ["a"],
+    given: ["b"],
   },
-  59: {
-    correct: ["1:4", "2:3", "3:2", "4:1"],
-    given: ["1:4", "2:3", "3:2", "4:1"],
-  },
-  60: {
-    correct: ["monitor", "marketplace", "advisor"],
-    given: ["monitor", "marketplace", "advisor"],
-  },
-  61: {
+  105: {
     correct: ["a"],
     given: ["a"],
   },
-  62: {
-    correct: ["containers"],
-    given: ["containers"],
+  106: {
+    correct: [2],
+    given: [2],
   },
-  63: {
+  107: {
+    correct: ["d"],
+    given: ["d"],
+  },
+  108: {
+    correct: [1],
+    given: [1],
+  },
+  109: {
+    correct: ["a"],
+    given: ["a"],
+  },
+  110: {
+    correct: [1, 1, 1],
+    given: [1, 1, 1],
+  },
+  111: {
+    correct: [0, 1, 0],
+    given: [0, 1, 0],
+  },
+  112: {
     correct: ["b"],
     given: ["b"],
   },
-  64: {
-    correct: ["c"],
-    given: ["c"],
-  },
-  65: {
-    correct: ["azure cosmos db"],
-    given: ["azure cosmos db"],
-  },
-  66: {
+  113: {
     correct: ["a"],
     given: ["a"],
   },
-  67: {
+  114: {
+    correct: ["b"],
+    given: ["b"],
+  },
+  115: {
+    correct: ["c", "d"],
+    given: ["c", "d"],
+  },
+  116: {
+    correct: [0, 0, 1],
+    given: [0, 0, 1],
+  },
+  117: {
+    correct: ["d"],
+    given: ["d"],
+  },
+  118: {
+    correct: ["1:4", "2:3", "3:1", "4:2"],
+    given: ["1:2", "2:4", "3:1", "4:3"],
+  },
+  119: {
+    correct: ["a"],
+    given: ["c"],
+  },
+  120: {
+    correct: ["b"],
+    given: ["b"],
+  },
+  121: {
     correct: ["c"],
     given: ["c"],
   },
-  68: {
+  122: {
+    correct: ["b"],
+    given: ["b"],
+  },
+  123: {
+    correct: [1, 1, 1],
+    given: [1, 1, 1],
+  },
+  124: {
+    correct: [3],
+    given: [3],
+  },
+  125: {
+    correct: ["a"],
+    given: ["a"],
+  },
+  126: {
+    correct: ["lock"],
+    given: ["lock"],
+  },
+  127: {
     correct: ["c"],
     given: ["c"],
   },
-  69: {
+  128: {
+    correct: ["d"],
+    given: ["d"],
+  },
+  129: {
+    correct: ["b"],
+    given: ["b"],
+  },
+  130: {
+    correct: ["c"],
+    given: ["c"],
+  },
+  131: {
+    correct: [0, 0, 0],
+    given: [0, 0, 0],
+  },
+  132: {
+    correct: [],
+    given: ["azure information protection"],
+  },
+  133: {
     correct: [],
     given: [],
   },
-  70: {
+  134: {
     correct: [],
     given: [],
   },
-  71: {
+  135: {
     correct: [],
     given: [],
   },
-  72: {
+  136: {
     correct: [],
     given: [],
   },
-  73: {
+  137: {
     correct: [],
     given: [],
   },
-  74: {
+  138: {
     correct: [],
     given: [],
   },
-  75: {
+  139: {
     correct: [],
     given: [],
   },
-  76: {
+  140: {
     correct: [],
     given: [],
   },
-  77: {
+  141: {
     correct: [],
     given: [],
   },
-  78: {
+  142: {
     correct: [],
     given: [],
   },
-  79: {
+  143: {
     correct: [],
     given: [],
   },
-  80: {
+  144: {
     correct: [],
     given: [],
   },
-  81: {
+  145: {
     correct: [],
     given: [],
   },
-  82: {
+  146: {
     correct: [],
     given: [],
   },
-  83: {
+  147: {
     correct: [],
     given: [],
   },
-  84: {
+  148: {
     correct: [],
     given: [],
   },
-  85: {
+  149: {
     correct: [],
     given: [],
   },
-  86: {
-    correct: [],
-    given: [],
-  },
-  87: {
-    correct: [],
-    given: [],
-  },
-  88: {
-    correct: [],
-    given: [],
-  },
-  89: {
-    correct: [],
-    given: [],
-  },
-  90: {
-    correct: [],
-    given: [],
-  },
-  91: {
-    correct: [],
-    given: [],
-  },
-  92: {
-    correct: [],
-    given: [],
-  },
-  93: {
-    correct: [],
-    given: [],
-  },
-  94: {
-    correct: [],
-    given: [],
-  },
-  95: {
-    correct: [],
-    given: [],
-  },
-  96: {
-    correct: [],
-    given: [],
-  },
-  97: {
-    correct: [],
-    given: [],
-  },
-  98: {
-    correct: [],
-    given: [],
-  },
-  99: {
-    correct: [],
-    given: [],
-  },
-  100: {
+  150: {
     correct: [],
     given: [],
   },
